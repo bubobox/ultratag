0049563cae8baeffce9ff374836989c0fc00ff87
Jorgen Evens <jorgen@evens.eu>
2013-09-28 14:57:20 +0200

    Read JPEG using markers

--- poc/jorgen/js/main.js
+++ poc/jorgen/js/main.js
@@ -31,16 +31,24 @@
 	}
 
 	function parseData( data ) {
+		data = new Uint8Array( data );
 		var length = data.length;
 		var skip = 0;
 		var i = 0;
 		var b = 0;
+		var next = 0;
 
 		for( i=0; i<length; i++ ) {
 			b = ( b << 8 ) & 0xFFFF; // shift byte, truncate data
 			b += data[i]; // add new data;
 
+			if( i == 100 )
+				return;
+
+			//console.log( i, b );
+
 			if( b >= 0xFFD8 && b <= 0XFFFE ) {
+				console.log( 'marker found', b.toString(16) );
 				if( b == 0xFFD8 )
 					continue;
 				if( b == 0xFFD9 )
@@ -52,23 +60,38 @@
 					continue;
 				}
 
-				if( b == 0xFFE4 )
+				if( b == 0xFFE1 && next == 1 )
 					break;
+
+				if( b == 0xFFE1 && next < 1 )
+					next++;
 
 				skip = ( data[i+1] << 8 ) + data[i+2] + 2;
 				i += skip - 1;
 			}
 		}
 
-		skip = ( data[i+1] << 8 ) + data[i+2] + 2;
+		if( typeof data[i] == 'undefined' )
+			return;
 
-		i += 2;
+		skip = ( data[i+1] << 8 ) + data[i+2] - 2;
 
-		string = '';
+		i+=3;
+
+		s = '';
+		var i2 = 0;
+		console.log( 'skip', skip );
 		for( ; skip--; i++ ) {
-			string += String.fromCharCode( data[i] );
+			s += String.fromCharCode( data[i] );
 		}
 
-		console.log( string );
+		console.error( s );
+
+		s = s.substring( s.indexOf( '<' ) );
+
+		var parser = new DOMParser();
+		xmlDoc = parser.parseFromString( s, 'text/xml' );
+
+		console.info( xmlDoc );
 	};
 }());
