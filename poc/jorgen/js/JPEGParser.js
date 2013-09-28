002d78108750e9beebce5f2a3e14b53ff135ebe1
Jorgen Evens <jorgen@evens.eu>
2013-09-28 16:54:28 +0200

    Move JPEGParser to it's own file

---
+++ poc/jorgen/js/JPEGParser.js
@@ -0,0 +1,131 @@
+/**
+ * Parses JPEG file format
+ */
+var JPEGParser = function() {
+	this._listeners = {};
+	this.on( 'marker', this._onregion.bind( this ) );
+};
+
+JPEGParser.prototype = {
+
+	_listeners: null,
+
+	on: function( e, listener ) {
+		if( !this._listeners[e] )
+			this._listeners[e] = [];
+
+		this._listeners[e].push( listener );
+	},
+
+	trigger: function() {
+		var arg = [].slice.call( arguments, 0 ),
+			e = arg.shift(),
+			i = 0,
+			listeners = this._listeners[ e ];
+
+		for( i in listeners ) {
+			listeners[i].apply( null, arg );
+		}
+	},
+
+	/**
+	 * Parse a binary buffer that contains a JPEG
+	 * 
+	 * @param  Uint8Array data
+	 */
+	parse: function( /* Uint8Array */ data ) {
+		var length = data.length,
+			skip = 0,
+			i = 0,
+			b = 0;
+
+		for( i=0; i<length; i++ ) {
+			b = ( b << 8 ) & 0xFFFF; // shift byte, truncate data
+			b += data[i]; // add new data;
+
+			// Valid markers
+			if( b >= 0xFFD8 && b <= 0XFFFE ) {
+				if( b == 0xFFD8 ||
+					b == 0xFFD9 ||
+					( b >= 0xFFD0 && b <= 0xFFD7 ) ||
+					b == 0xFFDD ) {
+					this.trigger( 'marker', b, data, i+1, 0 );
+					continue;
+				}
+
+				skip = ( data[i+1] << 8 ) + data[i+2];
+
+				this.trigger( 'marker', b, data, i+3, skip-2 );
+
+				i += skip - 1 + 2;
+			}
+		}
+	},
+
+	/**
+	 * Parses the block followed by the marker.
+	 * 
+	 * @param  int marker 
+	 * @param  Uint8Array data
+	 * @param  int start 
+	 * @param  int length 
+	 */
+	_onregion: function( marker, data, start, length ) {
+		if( marker == 0xFFE1 ) {
+			var id = '';
+			id += String.fromCharCode( data[start] ) +
+				String.fromCharCode( data[start+1] ) +
+				String.fromCharCode( data[start+2] ) +
+				String.fromCharCode( data[start+3] );
+
+			if( id == 'Exif' )
+				return this.trigger( 'exif', data, start, length );
+
+			if( id == 'http' )
+				return this.trigger( 'xmp', this._parsers.xmp( data, start, length ), {
+					data: data,
+					start: start,
+					length: length
+				});
+
+			return;
+		}
+	},
+
+	_parsers: {
+		/**
+		 * Parses XMP data
+		 * 
+		 * @param  Uint8Array   Data Binary image.
+		 * @param  int start    First byte offset
+		 * @param  int length   Length of region
+		 * @return DOMDocument  XML DOM Documenten of XMP data
+		 */
+		xmp: function( data, start, length ) {
+			var s = '',
+				i = 0,
+				dom = new DOMParser();
+
+			for( i=0; i<length; i++ ) {
+				s += String.fromCharCode( data[start+i] );
+			}
+
+			s = s.substring( s.indexOf( '<' ) );
+
+			return dom.parseFromString( s );
+		},
+
+		/**
+		 * Parses Exif data
+		 * 
+		 * @param  Uint8Array   Data Binary image.
+		 * @param  int start    First byte offset
+		 * @param  int length   Length of region
+		 * @return Object 		Key-value pairs of data
+		 */
+		exif: function( data, start, length ) {
+
+		}
+	}
+
+};
--- poc/jorgen/js/main.js
+++ poc/jorgen/js/main.js
@@ -35,97 +35,10 @@
 
 		var parser = new JPEGParser();
 		parser.on( 'xmp', function( data, start, length ) {
-			var s = '',
-				i = 0,
-				dom;
-
-			for( i=0; i<length; i++ ) {
-				s += String.fromCharCode( data[start+i] );
-			}
-
-			s = s.substring( s.indexOf( '<' ) );
-
-			dom = new DOMParser();
-			doc = dom.parseFromString( s, 'text/xml' );
-			console.log( doc );
+			
 		});
 
 		parser.parse( data );
 	};
 }());
 
-var JPEGParser = function() {
-	this._listeners = {};
-	this.on( 'marker', this._onregion.bind( this ) );
-};
-
-JPEGParser.prototype = {
-
-	_listeners: null,
-
-	on: function( e, listener ) {
-		if( !this._listeners[e] )
-			this._listeners[e] = [];
-
-		this._listeners[e].push( listener );
-	},
-
-	trigger: function() {
-		var arg = [].slice.call( arguments, 0 ),
-			e = arg.shift(),
-			i = 0,
-			listeners = this._listeners[ e ];
-
-		for( i in listeners ) {
-			listeners[i].apply( null, arg );
-		}
-	},
-
-	parse: function( /* Uint8Array */ data ) {
-		var length = data.length,
-			skip = 0,
-			i = 0,
-			b = 0;
-
-		for( i=0; i<length; i++ ) {
-			b = ( b << 8 ) & 0xFFFF; // shift byte, truncate data
-			b += data[i]; // add new data;
-
-			// Valid markers
-			if( b >= 0xFFD8 && b <= 0XFFFE ) {
-				if( b == 0xFFD8 ||
-					b == 0xFFD9 ||
-					( b >= 0xFFD0 && b <= 0xFFD7 ) ||
-					b == 0xFFDD ) {
-					this.trigger( 'marker', b, data, i+1, 0 );
-					continue;
-				}
-
-				skip = ( data[i+1] << 8 ) + data[i+2];
-
-				this.trigger( 'marker', b, data, i+3, skip-2 );
-
-				i += skip - 1 + 2;
-			}
-		}
-	},
-
-	_onregion: function( marker, data, start, length ) {
-		if( marker == 0xFFE1 ) {
-			var id = '';
-			id += String.fromCharCode( data[start] ) +
-				String.fromCharCode( data[start+1] ) +
-				String.fromCharCode( data[start+2] ) +
-				String.fromCharCode( data[start+3] );
-
-			if( id == 'Exif' )
-				return this.trigger( 'exif', data, start, length );
-
-			if( id == 'http' )
-				return this.trigger( 'xmp', data, start, length );
-
-			return;
-		}
-	}
-
-};
