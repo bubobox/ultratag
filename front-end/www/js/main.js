f80a1e4c3afeb4ca7bb6c8247970491111f90666
Wim Mostmans <wim@sitebase.be>
2013-09-29 11:54:49 +0200

    basic implementation of front end

--- front-end/www/js/main.js
+++ front-end/www/js/main.js
@@ -1,6 +1,35 @@
 /*jslint vars: true, devel: true */
 /*global $: false */
 
+/*
+(function() {
+    var zone = document.querySelector( '.drag-drop' );
+
+    zone.addEventListener( 'drop', function( e ) {
+        var files = e.dataTransfer.files,
+            count = files.length;
+
+        for( i=0; i<count;i++ )
+            console.log( files[i] );
+
+        e.preventDefault();
+        e.cancel = true;
+
+        readFile( files[0] );
+    });
+
+    function readFile( file ) {
+        var reader = new FileReader();
+        console.error(file);
+        reader.onloadend = function( e ) {
+            //parseData( e.currentTarget.result );
+        }
+
+        reader.readAsArrayBuffer( file );
+    }
+
+}());*/
+
 // Mobile browser detection
 var isMobile = {
     Android: function() {
@@ -31,6 +60,8 @@ $(function(){
     // flickering to occur as `dragleave` is fired constantly.
     var dragTimer;
     $(document).on('dragover', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
         var dt = e.originalEvent.dataTransfer;
         if(dt.types != null && (dt.types.indexOf ? dt.types.indexOf('Files') != -1 : dt.types.contains('application/x-moz-file'))) {
             $(".drag-drop").addClass('visible');
@@ -38,15 +69,94 @@ $(function(){
         }
     });
     $(document).on('dragleave', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
         dragTimer = window.setTimeout(function() {
             $(".drag-drop").removeClass('visible');
         }, 25);
     });
 
+    $(document).on('drop', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        $(".drag-drop").removeClass('visible');
+        if(e.originalEvent.dataTransfer){
+            var file = e.originalEvent.dataTransfer.files[0];
+
+            if(file.type !== 'image/jpeg') {
+                alert('For the moment we only support jpeg images.');
+            }
+
+            readFile(file);
+        }
+    });
+
+    function readFile( file ) {
+        var reader = new FileReader();
+        useFile(file);
+        reader.onloadend = function( e ) {
+            parseData( e.currentTarget.result );
+        }
+        reader.readAsArrayBuffer( file );
+    }
+
+    function parseData( data ) {
+        view('info');
+        data = new Uint8Array( data );
+        var parser = new JPEGParser();
+        parser.on( 'xmp', function( data, start, length ) {
+            console.error('---', data, data.get());
+        });
+        parser.on( 'exif', function( data ) {
+            var table = createTable(data);
+            $('.exif .table-wrapper').html(table);
+        })
+        parser.parse( data );
+    };
+
+    function useFile(file) {
+
+        // Set file as preview
+        var data = URL.createObjectURL(file);
+        document.getElementById('preview').src = data;
+        console.log(file);
+        $('.info #name').html(file.name);
+        $('.info #width').html(file.width);
+        $('.info #height').html(file.height);
+        $('.info #size').html(file.size);
+        $('.info #modified').html(file.lastModifiedDate);
+        $('.info #mime').html(file.type);
+    }
+
+    // Bind triggers for tabs
+    $('[data-trigger=tab]').on('click', function() {
+        tab($(this).data('tab'));
+    });
+
+    function tab(idx) {
+        $('[data-trigger=tab][data-tab=' + idx + ']').addClass('active').siblings().removeClass('active');
+        $('.tab').hide();
+        $('#tab' + idx).show();
+    }
+
+    function view(name) {
+        $('.view').hide();
+        $('.' + name).show();
+    }
+
     // Tinynav 
     $(".tabs").tinyNav({
         active: 'active' // String: Set the "active" class
     });
+
+    function createTable(data)
+    {
+        var html = '';
+        for(var idx in data) {
+            html += '<tr><td>' + data[idx].key + '</td><td>' + data[idx].value() + '</td></tr>';
+        }
+        return '<table width="100%"><tbody>' + html + '</tbody></table>';
+    }
 
     // set helper
     $('.select-helper span').html($(this).find('option:selected').html());
